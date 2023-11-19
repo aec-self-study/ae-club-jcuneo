f29db099a0484ac054814d52d6948e14a2b2dd5a
Jack Cuneo <46851876+jackcuneo@users.noreply.github.com>
2023-11-19 16:29:46 +0000

    initial commit

---
+++ customers.sql
@@ -0,0 +1,18 @@
+WITH orders_by_cust AS (
+  SELECT
+    customer_id
+   ,COUNT(DISTINCT id) AS count_orders
+   ,MIN(created_at) AS min_order_created_at
+  FROM `analytics-engineers-club.coffee_shop.orders`
+  GROUP BY customer_id
+)
+
+SELECT
+  c.*
+ ,o.count_orders
+ ,o.min_order_created_at
+FROM `analytics-engineers-club.coffee_shop.customers` AS c
+  LEFT JOIN orders_by_cust AS o
+    ON o.customer_id = c.id
+ORDER BY min_order_created_at
+LIMIT 5
